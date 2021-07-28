f1d2416f9e7e57cc43325551050f2de65bfc37f4
tubs <tubs33959@gmail.com>
2021-07-29 06:24:38 +0800

    Updated Project Files

---
+++ docs/update.bat
@@ -0,0 +1,30 @@
+cd C:\Users\smd153\Desktop\Task\03.Web Development\apps\dreamdormitory
+git config --global user.name "tubs"
+git config --global user.email "tubs33959@gmail.com"
+git stash
+git pull origin master
+git stash pop
+exit
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
